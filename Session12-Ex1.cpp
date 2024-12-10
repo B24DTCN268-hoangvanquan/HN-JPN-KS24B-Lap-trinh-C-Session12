43e3ff8798c7efdd78166e82d5c67b546481fe22
B24DTCN268-hoangvanquan <hoangvanquan1154@gmail.com>
2024-12-10 14:51:07 +0700

    bài tập ss12-1

---
+++ Session12-Ex1.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int sum(int a,int b){
+	return a+b;
+}
+int main(){
+	int a,b;
+	scanf("%d %d",&a,&b);
+	int tong=sum(a,b);
+	
+	printf("tong 2 so la : %d",tong);
+	return 0; 
+}
